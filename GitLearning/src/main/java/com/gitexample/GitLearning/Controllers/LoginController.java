b1c79d66e83c6393b9a3dc85c38c1226f0d68596
Ankit Chauhan <you@example.com>
2025-05-13 11:31:01 +0530

    Login Branch

---
+++ GitLearning/src/main/java/com/gitexample/GitLearning/Controllers/LoginController.java
@@ -0,0 +1,13 @@
+package com.gitexample.GitLearning.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+    
+    @GetMapping("Login")
+    private String login(){
+        return null;
+    }
+}
